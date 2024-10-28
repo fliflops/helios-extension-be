6356dbc1f6c55d30f1b97fda1028c8634fd0a138
fliflops <carlo.vincnt@gmail.com>
2024-10-28 15:00:45 +0800

    first commit

---
+++ src/api/models/helios/trip_plan_hdr_tbl.js
@@ -0,0 +1,25 @@
+const {Model, DataTypes} = require('sequelize')
+
+class trip_plan_hdr_tbl extends Model {
+    static init(sequelize) {
+        return super.init({
+            tripPlanNo:{
+                type: DataTypes.STRING,
+                primaryKey:true
+            },
+            actual_vendor: DataTypes.STRING,
+            actual_vehicle_id: DataTypes.STRING,
+            actual_vehicle_type: DataTypes.STRING,
+            cleared_by:  DataTypes.STRING,       
+            date_cleared: DataTypes.STRING,      
+            tripStatus:   DataTypes.STRING      
+        },
+        {
+            sequelize,
+            tableName:'trip_plan_hdr_tbl',
+            timestamps: false
+        })
+    }
+}
+
+module.exports = trip_plan_hdr_tbl;
